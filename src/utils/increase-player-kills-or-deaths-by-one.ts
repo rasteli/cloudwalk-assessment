c22b78b3b50a1341e24dae7a5474b023717fcecf
Gabriel Rasteli <gabrielrasteli3@gmail.com>
2022-10-05 21:08:31 -0300

    updated increase-player-kills-or-deaths-by-one.ts

--- src/utils/increase-player-kills-or-deaths-by-one.ts
+++ src/utils/increase-player-kills-or-deaths-by-one.ts
@@ -9,8 +9,11 @@ export function increasePlayerKillsOrDeathsByOne(
 ) {
   const playerInRanking = playerRanking[player]
 
+  // If player is in the ranking and there is prop, increments it by 1
+  // else starts it with value 1
+
   return {
     ...playerInRanking,
-    [prop]: playerInRanking ? (playerInRanking[prop] ? playerInRanking[prop] + 1 : 1) : 1
+    [prop]: playerInRanking && playerInRanking[prop] ? playerInRanking[prop] + 1 : 1
   }
 }
